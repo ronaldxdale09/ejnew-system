e947c7cbb8d29a51fc59fa57c45659ca5492273b
FranciscoJamesKheil <bg201803128@wmsu.edu.ph>
2022-08-04 20:28:09 +0800

    changebtncolorExpense

--- ledger/modal/modal_expenses.php
+++ ledger/modal/modal_expenses.php
@@ -178,7 +178,7 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" name="submit" class="btn btn-primary">Save changes</button>
+                        <button type="submit" name="submit" class="btn btn-success">Save changes</button>
                     </div>
                 </form>
             </div>
