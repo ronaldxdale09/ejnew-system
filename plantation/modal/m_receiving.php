6f385bc186d3c3bafbf4d5a85dcb42498f86c2e7
Ronald Dale Fuentebella <72976699+ronaldxdale09@users.noreply.github.com>
2023-03-29 12:11:28 +0800

    .

--- plantation/modal/m_receiving.php
+++ plantation/modal/m_receiving.php
@@ -231,3 +231,112 @@ $('#newReceiving').on('shown.bs.modal', function() {
     $('.source', this).chosen();
 });
 </script>
+
+
+
+<div class="modal fade" id="modal_process" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Rubber | Purchase Contract</h5>
+                <button type="button" class="btn" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="function/transaction_new.php" method="POST">
+                    <!-- ... START -->
+                    <div class="form-group">
+                        <div class="row no-gutters">
+                            <input type="date" id="date" name="date" class='datepicker' value='<?php echo $dateNow?>'
+                                required>
+                        </div>
+                        <!--end  -->
+                        <div class="col-6 col-md-6">
+
+                        </div>
+                        <!--  end-->
+                    </div>
+            </div>
+            <center>
+                <div class="form-group">
+                    <div class="row no-gutters">
+                        <div class="col-6 col-md-6">
+                            <div class="input-group mb-12">
+                                <label class="col-md-12">Seller</label>
+                                <div class="col-md-12">
+                                    <select required="required" class='contact_seller col-md-12' name='name' id='name'>
+                                        <option disabled="disabled" selected="selected" value="">Select Seller
+                                        </option>
+                                        <?php echo $sellerList; ?>
+                                    </select>
+                                </div>
+
+                            </div>
+                        </div>
+                        <!--end  -->
+                        <div class="col-6 col-md-6">
+                            <div class="input-group mb-1">
+                                <label class="col-md-12">Type</label>
+                                <div class="col-md-12">
+                                    <select required="required" class='contact_seller col-md-12' name='type' id='type'>
+                                        <option disabled="disabled" selected="selected" value="">Select Type
+                                        </option>
+                                        <option value="WET">WET</option>
+                                        <option value="BALES">BALES</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <!--  end-->
+                    </div>
+                </div>
+            </center>
+            <br>
+            <div class="form-group">
+                <div class="row no-gutters">
+                    <div class="col-12 col-sm-5 col-md-12">
+                        <!--  -->
+                        <div class="input-group mb-1">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="inputGroup-sizing-default"
+                                    style='color:black;font-weight: bold;'>Contract Quantity</span>
+                            </div>
+                            <input type="text" style='text-align:right' name='quantity' id='quantity'
+                                class="form-control" onkeypress="return CheckNumeric()" onkeyup="FormatCurrency(this)"
+                                required>
+                            <div class="input-group-append">
+                                <span class="input-group-text">Kg</span>
+                            </div>
+                        </div>
+                        <!--  -->
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="form-group">
+                <div class="row no-gutters">
+                    <div class="col-12 col-sm-5 col-md-12">
+                        <!--  -->
+                        <div class="input-group mb-1">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="inputGroup-sizing-default"
+                                    style='color:black;font-weight: bold;'>₱/KG</span>
+                            </div>
+                            <input type="text" style='text-align:right' name='ca' id='ca' class="form-control"
+                                onkeypress="return CheckNumeric()" onkeyup="FormatCurrency(this)">
+                        </div>
+                        <!--  -->
+                    </div>
+                </div>
+            </div>
+            <!-- END -->
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" name='new_contract' class="btn btn-success text-white">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
