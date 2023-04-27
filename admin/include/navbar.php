46aa2354a4f3658e5a0c5ee5d1623f8a6d479ab0
Ronald Dale Fuentebella <72976699+ronaldxdale09@users.noreply.github.com>
2023-04-27 12:23:51 +0800

    nav

--- admin/include/navbar.php
+++ admin/include/navbar.php
@@ -1,40 +1,67 @@
 <nav id='navbar'>
     <div id='toggle-nav-btn'>
-        <i class='fa-solid fa-ellipsis'></i>
-    </div>
-    <div class='nav-title' style='font-weight:bold;'>
-        <img src='assets/img/logo.png' alt='Q-cart Logo' width='35' height='35' style='margin-right:5px;'> <span
-            class='nav-text'>EJN RUBBER</span>
+        <div class='nav-title' style='font-weight:bold;'>
+            <img src='assets/img/logo.png' alt='Q-cart Logo' width='35' height='35' style='margin-right:5px;'> <span
+                class='nav-text'>EJN RUBBER</span>
+        </div>
     </div>
 
-    <br>
-    <hr style='color:gray'>
-    
+
     <a class='nav-link' href='dashboard.php'>
-        <i class='fa-solid fa-house'></i> <span class='nav-text'>Home</span>
+        <i class='fas fa-home'></i> <span class='nav-text'>Home</span>
+    </a>
+
+    <a class='nav-link' href='admin_expenses.php'>
+        <i class='fas fa-cash-register'></i> <span class='nav-text'>Operating Expenses</span>
+    </a>
+
+    <a class='nav-link' href='9'>
+        <i class='fas fa-file-alt'></i> <span class='nav-text'>Reports</span>
     </a>
 
     <hr style='color:gray'>
 
 
-    <a class='nav-link' href='recording.php'>
-        <i class='fa-solid fa-cash-register'></i> <span class='nav-text'>Rubber Processing</span>
+    <a class='nav-link' href='admin_basilan_rubber.php'>
+        <i class='fas fa-warehouse'></i> <span class='nav-text'>Basilan Rubber</span>
+    </a>
+    <a class='nav-link' href='inv_bale.php' style='margin-left: 40px; font-size: 18px;'>
+        <span class='nav-text'>Bales Inventory</span>
+    </a>
+    <a class='nav-link' href='inv_cuplump.php' style='margin-left: 40px; font-size: 18px;'>
+        <span class='nav-text'>Cuplumps Inventory</span>
+    </a>
+    <a class='nav-link' href='record_allrubber.php' style='margin-left: 40px; font-size: 18px;'>
+        <span class='nav-text'>Transaction Record</span>
     </a>
 
-    <a class='nav-link' href='record_allrubber.php'>
-        <i class='fa-solid fa-book'></i> <span class='nav-text'>Transaction Record</span>
+
+    <a class='nav-link' href='admin_kidapawan_rubber.php'>
+        <i class='fas fa-tree'></i> <span class='nav-text'>Kidapawan Rubber</span>
+    </a>
+
+    <a class='nav-link'>
+        <i class='fas fa-truck'></i> <span class='nav-text'>Copra</span>
+    </a>
+
+    <a class='nav-link'>
+        <i class='fas fa-coffee'></i> <span class='nav-text'>Coffee</span>
     </a>
 
 
-  
 
-    <div class='logout-container'> 
+
+    <hr style='color:gray'>
+
+
+    <a class='nav-link' href='admin_users'>
+        <i class='fas fa-users'></i> <span class='nav-text'>Manage Users</span>
+    </a>
+
     <a class='nav-text' href='function/logout.php'>
-        
-        <span class='nav-text' >Logout  </span>
-        
-              <i class='fa-solid fa-arrow-right-to-bracket'></i>
-        </a>
-    </div>
+        <i class='fa-solid fa-arrow-right-to-bracket'></i>
+        <span class='nav-text'>Logout</span>
+    </a>
+
 </nav>
 <script src='assets/js/navbar.js'></script>
--- sales/include/navbar.php
+++ sales/include/navbar.php
@@ -1,12 +1,12 @@
 <nav id='navbar'>
 
     <div id='toggle-nav-btn'>
-    <div class='nav-title' style='font-weight:bold;'>
-        <img src='assets/img/logo.png' alt='Q-cart Logo' width='35' height='35' style='margin-right:5px;'> <span
-            class='nav-text'>EJN RUBBER</span>
+        <div class='nav-title' style='font-weight:bold;'>
+            <img src='assets/img/logo.png' alt='Q-cart Logo' width='35' height='35' style='margin-right:5px;'> <span
+                class='nav-text'>EJN RUBBER</span>
+        </div>
     </div>
-    </div>
-    
+
     <a class='nav-link' href='dashboard.php'>
         <i class='fa-solid fa-house'></i> <span class='nav-text'>Home</span>
     </a>
@@ -14,42 +14,34 @@
         <i class='fa-solid fa-truck'></i> <span class='nav-text'>Inventory</span>
     </a>
     <a class='nav-link' href='inv_bale.php' style='margin-left: 40px; font-size: 18px;'>
-    <span class='nav-text'>Bales</span>
-</a>
-<a class='nav-link' href='inv_cuplump.php' style='margin-left: 40px; font-size: 18px;'>
-    <span class='nav-text'>Cuplumps</span>
-</a>
-
-    
-
-    <!-- PURCHASE -->
+        <span class='nav-text'>Bales</span>
+    </a>
+    <a class='nav-link' href='inv_cuplump.php' style='margin-left: 40px; font-size: 18px;'>
+        <span class='nav-text'>Cuplumps</span>
+    </a>
 
 
     <hr style='color:gray'>
-    <!-- SALE -->
+
     <a class='nav-link' href='sales_bales.php'>
-        <i class='fa-solid fa-chart-line'></i> <span class='nav-text'>Bales Sales</span>
+        <i class='fa-solid fa-chart-line'></i> <span class='nav-text'>Bale Sales</span>
     </a>
 
     <a class='nav-link' href='sales_wet.php'>
-        <i class='fa-solid fa-chart-line'></i> <span class='nav-text'>Wet Export</span>
+        <i class='fa-solid fa-chart-line'></i> <span class='nav-text'>Cuplump Export</span>
     </a>
 
     <a class='nav-link' href='sales_record.php'>
         <i class='fa-solid fa-book'></i> <span class='nav-text'>Sales Record</span>
     </a>
 
-<!-- OTHER -->
-
     <hr style='color:gray'>
-
-
 
     <a class='nav-link' href='reports.php'>
         <i class='fa-solid fa-file-alt'></i> <span class='nav-text'>Reports</span>
     </a>
     <hr style='color:gray'>
-  
+
 
     <div class='logout-container'>
         <span class='nav-text'></span>
