3497ed20114531c0635288c8f7e63e38a869a81a
Ronald Dale Fuentebella <72976699+ronaldxdale09@users.noreply.github.com>
2023-05-10 20:42:35 +0800

    .

--- ledger/ledger-expense.php
+++ ledger/ledger-expense.php
@@ -38,10 +38,7 @@ $expense_year = mysqli_fetch_array($getYearTotal);
 
 
 ?>
-<!-- Bootstrap -->
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
 <body>
     <!-- Rounded tabs -->
@@ -60,12 +57,7 @@ $expense_year = mysqli_fetch_array($getYearTotal);
         </div>
     </div>
 </body>
-<script type="text/javascript" src="https://cdn.datatables.net/plug-ins/1.12.1/api/sum().js"></script>
-<script src="ledgerTab/js/expenses.js"></script>
-<!-- Bootstrap script -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
-    crossorigin="anonymous"></script>
+
 
 </html>
 <?php
