9c08da83548d66ca834b23445f4f1785c1157274
Ronald Dale Fuentebella <72976699+ronaldxdale09@users.noreply.github.com>
2023-06-02 08:49:47 +0800

    .

--- sales/sales_modal/wet_modal_shipment.php
+++ sales/sales_modal/wet_modal_shipment.php
@@ -1,4 +1,4 @@
-<!-- create Table Row -->
+
 <div class="modal fade" id="newWetExport" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
     aria-hidden="true">
     <div class="modal-dialog modal-md  ">
@@ -6,7 +6,7 @@
             <div class="modal-header">
                 <h5 class="modal-title"> NEW | CUPLUMP SHIPMENT</h5>
             </div>
-            <form method='POST' action='function/wet_export_process.php'>
+            <form method='POST' action='cuplump_shipment.php'>
 
                 <div class="modal-body">
 
@@ -25,12 +25,7 @@
                                 <input type="text" class="form-control" name="user">
                             </div>
                         </div>
-
                     </div>
-
-
-
-
                     <div class="col-md-12">
                         <div class="mb-3">
                             <label for="product_name" class="form-label">Remarks</label>
@@ -45,49 +40,6 @@
                     <button type="submit" name='new' class="btn btn-success">Proceed</button>
                 </div>
             </form>
-        </div>
-    </div>
-</div>
-
-
-
-<div class="modal fade" id='inventoryModal' tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog modal-xl  ">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Selected Inventory</h5>
-                <button type="button" class="btn text-light close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-
-                <div id='inventory_table'> </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-
-    </div>
-</div>
-
-<div class="modal fade" id="confirmSalesModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Confirm Sales</h5>
-                <button type="button" class="btn-close text-light" data-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <p class="fs-5 text-center">Are you sure you want to confirm this transaction?</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary" onclick="submitForm()">Confirm</button>
-                <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Close</button>
-            </div>
         </div>
     </div>
 </div>
