cad928a291af99c9ccfd651b00c36c301c994965
Ronald Dale Fuentebella <72976699+ronaldxdale09@users.noreply.github.com>
2023-06-15 16:34:52 +0800

    Update coffee_sales.php

--- ledger/modal/coffee_sales.php
+++ ledger/modal/coffee_sales.php
@@ -1,6 +1,6 @@
     <div class="modal fade" id="newCoffeeSale" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
         aria-hidden="true">
-        <div class="modal-dialog modal-lg">
+        <div class="modal-dialog modal-xl">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title"> NEW | COFFEE SALE</h5>
@@ -40,31 +40,30 @@
                         <div class="card">
                             <div class="card-body">
                                 <div class="row" id="itemLines">
-                                    <div class="col-5">
+                                    <div class="col-4">
                                         Product
                                         <div class="input-group mb-3">
                                             <select class="form-select" name="product[]" style="width: 100px;">
                                                 <option>Select...</option>
                                                 <?php
-                            $sql = "SELECT coffee_product_description, coffee_product_price FROM coffee_products";
-                            $result = mysqli_query($con, $sql);
-                            if ($result) {
-                                while ($row = mysqli_fetch_assoc($result)) {
-                                    $productDescription = $row['coffee_product_description'];
-                                    $productPrice = $row['coffee_product_price'];
-                                    echo "<option value='$productDescription'>$productDescription</option>";
-                                }
-                            }
-                            ?>
+                                                    $sql = "SELECT coffee_product_description, coffee_product_price FROM coffee_products";
+                                                    $result = mysqli_query($con, $sql);
+                                                    if ($result) {
+                                                        while ($row = mysqli_fetch_assoc($result)) {
+                                                            $productDescription = $row['coffee_product_description'];
+                                                            $productPrice = $row['coffee_product_price'];
+                                                            echo "<option value='$productDescription' data-price='$productPrice'>$productDescription</option>";                                                        }
+                                                    }
+                                                ?>
                                             </select>
                                         </div>
                                     </div>
-                                    <div class="col-2">
+                                    <div class="col-3">
                                         Price
                                         <div class="input-group mb-3">
                                             <span class="input-group-text">₱</span>
                                             <input type="text" class="form-control" name="price[]" style="width: 100px;"
-                                                value="<?php echo $productPrice; ?>" readonly>
+                                                readonly>
                                         </div>
                                     </div>
                                     <div class="col-2">
@@ -77,8 +76,8 @@
                                         Amount
                                         <div class="input-group mb-3">
                                             <span class="input-group-text">₱</span>
-                                            <input type="text" class="form-control" name="amount[]" style="width: 100px;"
-                                                readonly>
+                                            <input type="text" class="form-control" name="amount[]"
+                                                style="width: 100px;" readonly>
                                         </div>
                                     </div>
                                 </div>
@@ -118,16 +117,36 @@
             </div>
         </div>
     </div>
+    <script>
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the select element
+    var selectElement = document.querySelector('select[name="product[]"]');
 
+    // Get the price input element
+    var priceInput = document.querySelector('input[name="price[]"]');
+
+    // Add event listener for change event
+    selectElement.addEventListener('change', function() {
+        // Get the selected option
+        var selectedOption = this.options[this.selectedIndex];
+
+        // Get the price value from the selected option's dataset
+        var productPrice = selectedOption.dataset.price;
+
+        // Set the price input value
+        priceInput.value = productPrice;
+    });
+});
+    </script>
 
 
     <script>
-        function addItemLine() {
-            const itemLine = document.createElement('div');
-            itemLine.classList.add('item-line');
-            itemLine.innerHTML = `
+function addItemLine() {
+    const itemLine = document.createElement('div');
+    itemLine.classList.add('item-line');
+    itemLine.innerHTML = `
                 <div class="row">
-                    <div class="col-5">
+                    <div class="col-4">
                         <div class="input-group mb-3">
                             <select class="form-select product-select" name="product[]" style="width: 100px;">
                                 <option>Select...</option>
@@ -145,7 +164,7 @@
                             </select>
                         </div>
                     </div>
-                    <div class="col-2">
+                    <div class="col-3">
                         <div class="input-group mb-3">
                             <span class="input-group-text">₱</span>
                             <input type="text" class="form-control price-field" name="price[]" style="width: 100px;" readonly>
@@ -165,56 +184,56 @@
                 </div>
             `;
 
-            const itemLines = document.getElementById('itemLines');
-            itemLines.appendChild(itemLine);
+    const itemLines = document.getElementById('itemLines');
+    itemLines.appendChild(itemLine);
 
-            const productSelect = itemLine.querySelector('.product-select');
-            const priceField = itemLine.querySelector('.price-field');
-            const unitField = itemLine.querySelector('input[name="unit[]"]');
+    const productSelect = itemLine.querySelector('.product-select');
+    const priceField = itemLine.querySelector('.price-field');
+    const unitField = itemLine.querySelector('input[name="unit[]"]');
 
-            productSelect.addEventListener('change', function() {
-            const selectedOption = this.options[this.selectedIndex];
-            const price = selectedOption.getAttribute('data-price');
-            priceField.value = price;
-            unitField.value = '';
-            recalculateLine.call(unitField);
-        });
+    productSelect.addEventListener('change', function() {
+        const selectedOption = this.options[this.selectedIndex];
+        const price = selectedOption.getAttribute('data-price');
+        priceField.value = price;
+        unitField.value = '';
+        recalculateLine.call(unitField);
+    });
 
-        unitField.addEventListener('input', function() {
-            recalculateLine.call(this);
-        });
+    unitField.addEventListener('input', function() {
+        recalculateLine.call(this);
+    });
 
-        updateTotalAmountDue();
-    }
+    updateTotalAmountDue();
+}
 
-    function recalculateLine() {
-        const itemLine = this.closest('.item-line');
-        const qty = parseFloat(this.value) || 0;
-        const priceField = itemLine.querySelector('.price-field');
-        const amountField = itemLine.querySelector('input[name="amount[]"]');
+function recalculateLine() {
+    const itemLine = this.closest('.item-line');
+    const qty = parseFloat(this.value) || 0;
+    const priceField = itemLine.querySelector('.price-field');
+    const amountField = itemLine.querySelector('input[name="amount[]"]');
 
-        const price = parseFloat(priceField.value) || 0;
-        const amount = qty * price;
-        amountField.value = amount.toFixed(2);
+    const price = parseFloat(priceField.value) || 0;
+    const amount = qty * price;
+    amountField.value = amount.toFixed(2);
 
-        updateTotalAmountDue();
-    }
+    updateTotalAmountDue();
+}
 
-        function updateTotalAmountDue() {
-            const amountFields = Array.from(document.querySelectorAll('input[name="amount[]"]'));
-            const totalAmountField = document.querySelector('input[name="coffee_total_amount"]');
-            const totalAmount = amountFields.reduce((total, field) => total + (parseFloat(field.value) || 0), 0);
+function updateTotalAmountDue() {
+    const amountFields = Array.from(document.querySelectorAll('input[name="amount[]"]'));
+    const totalAmountField = document.querySelector('input[name="coffee_total_amount"]');
+    const totalAmount = amountFields.reduce((total, field) => total + (parseFloat(field.value) || 0), 0);
 
-            totalAmountField.value = totalAmount.toFixed(2);
-            updateRemainingBalance();
-        }
+    totalAmountField.value = totalAmount.toFixed(2);
+    updateRemainingBalance();
+}
 
-        function updateRemainingBalance() {
-            const totalAmount = parseFloat(document.querySelector('input[name="coffee_total_amount"]').value) || 0;
-            const amountPaid = parseFloat(document.querySelector('input[name="coffee_paid"]').value) || 0;
-            const balanceField = document.querySelector('input[name="coffee_balance"]');
-            const balance = totalAmount - amountPaid;
+function updateRemainingBalance() {
+    const totalAmount = parseFloat(document.querySelector('input[name="coffee_total_amount"]').value) || 0;
+    const amountPaid = parseFloat(document.querySelector('input[name="coffee_paid"]').value) || 0;
+    const balanceField = document.querySelector('input[name="coffee_balance"]');
+    const balance = totalAmount - amountPaid;
 
-            balanceField.value = balance.toFixed(2);
-        }
+    balanceField.value = balance.toFixed(2);
+}
     </script>
